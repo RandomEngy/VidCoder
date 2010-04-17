824378498715aaca315fba55cb5bc7a147f1d021
RandomEngy <none@none>
2010-04-16 22:01:40 -0700

    Adding missing files for logging/x264 options.

---
+++ VidCoder/View/LogWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using VidCoder.Properties;
+
+namespace VidCoder.View
+{
+    /// <summary>
+    /// Interaction logic for LogWindow.xaml
+    /// </summary>
+    public partial class LogWindow : Window
+    {
+        public LogWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            this.SetPlacement(Properties.Settings.Default.LogWindowPlacement);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Settings.Default.LogWindowPlacement = this.GetPlacement();
+            Settings.Default.Save();
+        }
+
+        private void logTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.logTextBox.ScrollToEnd();
+        }
+    }
+}
---
+++ VidCoder/ViewModel/LogViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VidCoder.Model;
+using System.Windows.Input;
+
+namespace VidCoder.ViewModel
+{
+    public class LogViewModel : OkCancelDialogViewModel
+    {
+        private Logger logger;
+
+        private ICommand clearLogCommand;
+
+        public LogViewModel(Logger logger)
+        {
+            this.logger = logger;
+        }
+
+        public string LogText
+        {
+            get
+            {
+                return logger.LogText;
+            }
+        }
+
+        public ICommand ClearLogCommand
+        {
+            get
+            {
+                if (this.clearLogCommand == null)
+                {
+                    this.clearLogCommand = new RelayCommand(param =>
+                    {
+                        this.logger.ClearLog();
+                        this.NotifyPropertyChanged("LogText");
+                    });
+                }
+
+                return this.clearLogCommand;
+            }
+        }
+
+        public void RefreshLogs()
+        {
+            this.NotifyPropertyChanged("LogText");
+        }
+    }
+}
