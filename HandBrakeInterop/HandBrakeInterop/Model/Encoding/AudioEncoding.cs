beebafd5990af6a6195e535dd89643d89720e38e
RandomEngy <none@none>
2010-08-29 12:26:03 -0700

    Updating to latest Handbrake SVN.

--- HandBrakeInterop/HandBrakeInterop/Model/Encoding/AudioEncoding.cs
+++ HandBrakeInterop/HandBrakeInterop/Model/Encoding/AudioEncoding.cs
@@ -11,7 +11,16 @@ namespace HandBrake.Interop
 		public AudioEncoder Encoder { get; set; }
 		public int Bitrate { get; set; }
 		public Mixdown Mixdown { get; set; }
+
+		/// <summary>
+		/// Obsolete. Use SampleRateRaw instead.
+		/// </summary>
+		[Obsolete("This property is ignored and only exists for backwards compatibility. Use SampleRateRaw instead.")]
 		public string SampleRate { get; set; }
+
+		/// <summary>
+		/// Gets or sets the sample rate in Hz.
+		/// </summary>
 		public int SampleRateRaw { get; set; }
 		public double Drc { get; set; }
 	}
