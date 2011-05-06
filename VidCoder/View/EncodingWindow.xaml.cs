79fc575e8037d3d05828a97ba56530b7dec8417b
RandomEngy <none@none>
2011-05-05 18:30:28 -0700

    Breaking out view for Encoding panel tabs into UserControls.

--- VidCoder/View/EncodingWindow.xaml.cs
+++ VidCoder/View/EncodingWindow.xaml.cs
@@ -23,82 +23,11 @@ namespace VidCoder.View
 	/// </summary>
 	public partial class EncodingWindow : Window
 	{
-		private ObservableCollection<AudioEncodingViewModel> audioEncodings;
-
 		public EncodingWindow()
 		{
 			InitializeComponent();
 
 			this.tabControl.SelectedIndex = Settings.Default.EncodingDialogLastTab;
-
-			if (Settings.Default.ShowAudioTrackNameField)
-			{
-				var gridView = this.audioList.View as GridView;
-				gridView.Columns.Insert(
-					gridView.Columns.Count - 1,
-					new GridViewColumn
-					{
-						Header = "Name",
-						Width = 98,
-						CellTemplate = this.Resources["NameTemplate"] as DataTemplate
-					});
-			}
-		}
-
-		private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
-		{
-			var encodingDialogVM = e.NewValue as EncodingViewModel;
-			this.audioEncodings = encodingDialogVM.AudioEncodings;
-
-			foreach (AudioEncodingViewModel encodingVM in this.audioEncodings)
-			{
-				encodingVM.PropertyChanged += this.encodingVM_PropertyChanged;
-			}
-
-			this.audioEncodings.CollectionChanged += this.audioEncodings_CollectionChanged;
-		}
-
-
-		private void audioEncodings_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-		{
-			ResizeGridViewColumn(this.targetStreamColumn);
-
-			if (e.Action == NotifyCollectionChangedAction.Add)
-			{
-				foreach (AudioEncodingViewModel encodingVM in e.NewItems)
-				{
-					encodingVM.PropertyChanged += this.encodingVM_PropertyChanged;
-				}
-			}
-			else if (e.Action == NotifyCollectionChangedAction.Remove)
-			{
-				foreach (AudioEncodingViewModel encodingVM in e.OldItems)
-				{
-					encodingVM.PropertyChanged -= this.encodingVM_PropertyChanged;
-				}
-			}
-		}
-
-		private void encodingVM_PropertyChanged(object sender, PropertyChangedEventArgs e)
-		{
-			switch (e.PropertyName)
-			{
-				case "TargetStreamIndex":
-					ResizeGridViewColumn(this.targetStreamColumn);
-					break;
-				default:
-					break;
-			}
-		}
-
-		private static void ResizeGridViewColumn(GridViewColumn column)
-		{
-			if (double.IsNaN(column.Width))
-			{
-				column.Width = column.ActualWidth;
-			}
-
-			column.Width = double.NaN;
 		}
 
 		protected override void OnSourceInitialized(EventArgs e)
---
+++ VidCoder/View/Panels/AudioPanel.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VidCoder.Properties;
+using VidCoder.ViewModel;
+
+namespace VidCoder.View
+{
+	/// <summary>
+	/// Interaction logic for AudioPanel.xaml
+	/// </summary>
+	public partial class AudioPanel : UserControl
+	{
+		private ObservableCollection<AudioEncodingViewModel> audioEncodings;
+
+		public AudioPanel()
+		{
+			InitializeComponent();
+
+			if (Settings.Default.ShowAudioTrackNameField)
+			{
+				var gridView = this.audioList.View as GridView;
+				gridView.Columns.Insert(
+					gridView.Columns.Count - 1,
+					new GridViewColumn
+					{
+						Header = "Name",
+						Width = 98,
+						CellTemplate = this.Resources["NameTemplate"] as DataTemplate
+					});
+			}
+		}
+
+		private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			var encodingDialogVM = e.NewValue as EncodingViewModel;
+			this.audioEncodings = encodingDialogVM.AudioEncodings;
+
+			foreach (AudioEncodingViewModel encodingVM in this.audioEncodings)
+			{
+				encodingVM.PropertyChanged += this.encodingVM_PropertyChanged;
+			}
+
+			this.audioEncodings.CollectionChanged += this.audioEncodings_CollectionChanged;
+		}
+
+
+		private void audioEncodings_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			ResizeGridViewColumn(this.targetStreamColumn);
+
+			if (e.Action == NotifyCollectionChangedAction.Add)
+			{
+				foreach (AudioEncodingViewModel encodingVM in e.NewItems)
+				{
+					encodingVM.PropertyChanged += this.encodingVM_PropertyChanged;
+				}
+			}
+			else if (e.Action == NotifyCollectionChangedAction.Remove)
+			{
+				foreach (AudioEncodingViewModel encodingVM in e.OldItems)
+				{
+					encodingVM.PropertyChanged -= this.encodingVM_PropertyChanged;
+				}
+			}
+		}
+
+		private void encodingVM_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			switch (e.PropertyName)
+			{
+				case "TargetStreamIndex":
+					ResizeGridViewColumn(this.targetStreamColumn);
+					break;
+				default:
+					break;
+			}
+		}
+
+		private static void ResizeGridViewColumn(GridViewColumn column)
+		{
+			if (double.IsNaN(column.Width))
+			{
+				column.Width = column.ActualWidth;
+			}
+
+			column.Width = double.NaN;
+		}
+	}
+}
