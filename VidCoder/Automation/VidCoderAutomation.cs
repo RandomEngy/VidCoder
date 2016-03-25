eb6fba9cecf4bd04820b14cbfcff640a75374cfd
RandomEngy <david.rickard@gmail.com>
2016-03-25 12:34:34 -0700

    Changed automation service listener to lazy startup to save 500ms on startup time.

--- VidCoder/Automation/VidCoderAutomation.cs
+++ VidCoder/Automation/VidCoderAutomation.cs
@@ -14,61 +14,69 @@ namespace VidCoder.Automation
 		public void Encode(string source, string destination, string preset, string picker)
 		{
 			var processingService = Ioc.Get<ProcessingService>();
-
-			try
+			DispatchUtilities.Invoke(() =>
 			{
-				processingService.Process(source, destination, preset, picker);
-			}
-			catch (Exception exception)
-			{
-				throw new FaultException<AutomationError>(new AutomationError { Message = exception.Message });
-			}
+				try
+				{
+					processingService.Process(source, destination, preset, picker);
+				}
+				catch (Exception exception)
+				{
+					throw new FaultException<AutomationError>(new AutomationError { Message = exception.Message });
+				}
+			});
 		}
 
 		public void Scan(string source)
 		{
 			var mainVM = Ioc.Get<MainViewModel>();
-
-			try
+			DispatchUtilities.Invoke(() =>
 			{
-				mainVM.ScanFromAutoplay(source);
-			}
-			catch (Exception exception)
-			{
-				throw new FaultException<AutomationError>(new AutomationError { Message = exception.Message });
-			}
+				try
+				{
+					mainVM.ScanFromAutoplay(source);
+				}
+				catch (Exception exception)
+				{
+					throw new FaultException<AutomationError>(new AutomationError { Message = exception.Message });
+				}
+			});
 		}
 
 		public void ImportPreset(string filePath)
 		{
 			var presetImporter = Ioc.Get<IPresetImportExport>();
-
-			try
+			DispatchUtilities.Invoke(() =>
 			{
-				Preset preset = presetImporter.ImportPreset(filePath);
-				this.ShowMessage(string.Format(MainRes.PresetImportSuccessMessage, preset.Name));
-			}
-			catch (Exception exception)
-			{
-				this.ShowMessage(MainRes.PresetImportErrorMessage);
-				throw new FaultException<AutomationError>(new AutomationError { Message = exception.Message });
-			}
+				try
+				{
+					Preset preset = presetImporter.ImportPreset(filePath);
+					this.ShowMessage(string.Format(MainRes.PresetImportSuccessMessage, preset.Name));
+				}
+				catch (Exception exception)
+				{
+					this.ShowMessage(MainRes.PresetImportErrorMessage);
+					throw new FaultException<AutomationError>(new AutomationError { Message = exception.Message });
+				}
+			});
 		}
 
 		public void ImportQueue(string filePath)
 		{
 			var queueImporter = Ioc.Get<IQueueImportExport>();
-
-			try
+			DispatchUtilities.Invoke(() =>
 			{
-				queueImporter.Import(filePath);
-				this.ShowMessage(MainRes.QueueImportSuccessMessage);
-			}
-			catch (Exception exception)
-			{
-				this.ShowMessage(MainRes.QueueImportErrorMessage);
-				throw new FaultException<AutomationError>(new AutomationError { Message = exception.Message });
-			}
+				try
+				{
+					queueImporter.Import(filePath);
+					this.ShowMessage(MainRes.QueueImportSuccessMessage);
+				}
+				catch (Exception exception)
+				{
+					this.ShowMessage(MainRes.QueueImportErrorMessage);
+					throw new FaultException<AutomationError>(new AutomationError { Message = exception.Message });
+				}
+			});
 		}
 
 		private void ShowMessage(string message)
