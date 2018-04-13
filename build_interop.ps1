474a8bb378a81019e05a1194ac99753f6e3a7c99
RandomEngy <david.rickard@gmail.com>
2018-04-13 06:04:13 -0700

    Updated HandBrake core to 1.1.0 Stable.

--- build_interop.ps1
+++ build_interop.ps1
@@ -8,8 +8,9 @@ $HandBrakeFolder = "..\HandBrake\win\CS"
 
 $HandBrakeSolution = $HandBrakeFolder + "\HandBrake.sln"
 $HandBrakeAppServicesFolder = $HandBrakeFolder + "\HandBrake.ApplicationServices"
+$HandBrakeAppServiceProject = $HandBrakeAppServicesFolder + "\HandBrake.ApplicationServices.csproj"
 
-& $DevEnvExe $HandBrakeSolution /Rebuild "Release|x64" /project "HandBrake.ApplicationServices"; ExitIfFailed
+& $MsBuildExe $HandBrakeAppServiceProject /t:rebuild "/p:Configuration=Release;Platform=x64"; ExitIfFailed
 copy ($HandBrakeAppServicesFolder + "\bin\Release\HandBrake.ApplicationServices.dll") Lib -force
 copy ($HandBrakeAppServicesFolder + "\bin\Release\HandBrake.ApplicationServices.pdb") Lib -force
 "Files copied."
