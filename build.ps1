163777c82176bf585f64686eb076be8b0a5cd385
RandomEngy <david.rickard@gmail.com>
2018-05-06 22:22:35 -0700

    Fix beta flag after merge.

--- build.ps1
+++ build.ps1
@@ -117,7 +117,7 @@ function UpdateLatestJson($latestFile, $versionShort, $versionTag, $installerFil
 }
 
 # Master switch for if this branch is beta
-$beta = $false
+$beta = $true
 
 if ($debugBuild) {
     $buildFlavor = "Debug"
