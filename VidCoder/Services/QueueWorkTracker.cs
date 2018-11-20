f741285fa136ac33a7ec76e4b62aee088b6681ec
RandomEngy <david.rickard@gmail.com>
2018-11-20 00:03:13 -0800

    Added locks for the queue work tracker.

--- VidCoder/Services/QueueWorkTracker.cs
+++ VidCoder/Services/QueueWorkTracker.cs
@@ -23,6 +23,7 @@ namespace VidCoder.Services
 	    private TimeSpan overallEtaSpan;
 	    private bool diagnosticsLogged;
 	    private long completedItems;
+	    private readonly object trackerLock = new object();
 
 		/// <summary>
 		/// All work since starting encoding that has finished or is still in the queue.
@@ -66,23 +67,26 @@ namespace VidCoder.Services
 		/// </summary>
 		public void ReportEncodeStart(IEnumerable<JobWork> initialWork)
 		{
-			this.diagnosticsLogged = false;
-			this.CanShowEta = false;
-		    this.elapsedQueueEncodeTime = Stopwatch.StartNew();
-			this.OverallWorkCompletionRate = 0;
-			this.OverallEncodeProgressFraction = 0;
-			this.pollCount = 0;
-			this.completedQueueWork = 0.0;
-			this.completedItems = 0;
-
-			this.totalQueueCost = 0.0;
-
-			this.queuedWork.Clear();
-
-			foreach (var work in initialWork)
+			lock (this.trackerLock)
 			{
-				this.totalQueueCost += work.Cost;
-				this.queuedWork.Add(work);
+				this.diagnosticsLogged = false;
+				this.CanShowEta = false;
+				this.elapsedQueueEncodeTime = Stopwatch.StartNew();
+				this.OverallWorkCompletionRate = 0;
+				this.OverallEncodeProgressFraction = 0;
+				this.pollCount = 0;
+				this.completedQueueWork = 0.0;
+				this.completedItems = 0;
+
+				this.totalQueueCost = 0.0;
+
+				this.queuedWork.Clear();
+
+				foreach (var work in initialWork)
+				{
+					this.totalQueueCost += work.Cost;
+					this.queuedWork.Add(work);
+				}
 			}
 		}
 
@@ -91,109 +95,130 @@ namespace VidCoder.Services
 		/// </summary>
 		public void ReportEncodeStop()
 	    {
-		    this.elapsedQueueEncodeTime.Stop();
+			lock (this.trackerLock)
+			{
+				this.elapsedQueueEncodeTime.Stop(); 
+			}
 	    }
 
 	    public void ReportEncodePause()
 	    {
-		    this.elapsedQueueEncodeTime?.Stop();
+			lock (this.trackerLock)
+			{
+				this.elapsedQueueEncodeTime?.Stop(); 
+			}
 	    }
 
 	    public void ReportEncodeResume()
 	    {
-		    this.elapsedQueueEncodeTime?.Start();
+			lock (this.trackerLock)
+			{
+				this.elapsedQueueEncodeTime?.Start(); 
+			}
 	    }
 
 		public void ReportAddedToQueue(JobWork work)
 	    {
-			this.totalQueueCost += work.Cost;
-			this.queuedWork.Add(work);
+			lock (this.trackerLock)
+			{
+				this.totalQueueCost += work.Cost;
+				this.queuedWork.Add(work); 
+			}
 		}
 
 		public void ReportRemovedFromQueue(JobWork work)
 	    {
-		    this.totalQueueCost -= work.Cost;
-			this.queuedWork.Remove(work);
+			lock (this.trackerLock)
+			{
+				this.totalQueueCost -= work.Cost;
+				this.queuedWork.Remove(work); 
+			}
 		}
 
 		public void ReportFinished(JobWork work)
 	    {
-			this.completedQueueWork += work.Cost;
-		    this.completedItems++;
+			lock (this.trackerLock)
+			{
+				this.completedQueueWork += work.Cost;
+				this.completedItems++; 
+			}
 	    }
 
 		public void CalculateOverallEncodeProgress(double inProgressJobsCompletedWork)
 	    {
-			double totalCompletedWork = this.completedQueueWork + inProgressJobsCompletedWork;
+			lock (this.trackerLock)
+			{
+				double totalCompletedWork = this.completedQueueWork + inProgressJobsCompletedWork;
 
-		    this.OverallEncodeProgressFraction = this.totalQueueCost > 0 ? totalCompletedWork / this.totalQueueCost : 0;
+				this.OverallEncodeProgressFraction = this.totalQueueCost > 0 ? totalCompletedWork / this.totalQueueCost : 0;
 
-		    double queueElapsedSeconds = this.elapsedQueueEncodeTime.Elapsed.TotalSeconds;
-		    this.OverallWorkCompletionRate = queueElapsedSeconds > 0 ? totalCompletedWork / queueElapsedSeconds : 0;
+				double queueElapsedSeconds = this.elapsedQueueEncodeTime.Elapsed.TotalSeconds;
+				this.OverallWorkCompletionRate = queueElapsedSeconds > 0 ? totalCompletedWork / queueElapsedSeconds : 0;
 
-		    // Only update encode time every 5th update.
-		    if (Interlocked.Increment(ref this.pollCount) % 5 == 1)
-		    {
-			    if (!this.CanShowEta && this.elapsedQueueEncodeTime != null && queueElapsedSeconds > 0.5 && this.OverallEncodeProgressFraction != 0.0)
-			    {
-				    this.CanShowEta = true;
-			    }
+				// Only update encode time every 5th update.
+				if (Interlocked.Increment(ref this.pollCount) % 5 == 1)
+				{
+					if (!this.CanShowEta && this.elapsedQueueEncodeTime != null && queueElapsedSeconds > 0.5 && this.OverallEncodeProgressFraction != 0.0)
+					{
+						this.CanShowEta = true;
+					}
 
-			    if (this.CanShowEta)
-			    {
-				    if (this.OverallEncodeProgressFraction == 1)
-				    {
-					    this.EstimatedTimeRemaining = Utilities.FormatTimeSpan(TimeSpan.Zero);
-				    }
-				    else if (this.OverallEncodeProgressFraction >= 0 && this.OverallEncodeProgressFraction <= 1.01)
-				    {
-					    if (this.OverallEncodeProgressFraction == 0)
-					    {
-						    this.overallEtaSpan = TimeSpan.MaxValue;
-					    }
-					    else
-					    {
-						    try
-						    {
-							    this.overallEtaSpan =
-								    TimeSpan.FromSeconds((long)(((1.0 - this.OverallEncodeProgressFraction) * queueElapsedSeconds) / this.OverallEncodeProgressFraction));
-						    }
-						    catch (OverflowException)
-						    {
-							    this.overallEtaSpan = TimeSpan.MaxValue;
-						    }
-					    }
+					if (this.CanShowEta)
+					{
+						if (this.OverallEncodeProgressFraction == 1)
+						{
+							this.EstimatedTimeRemaining = Utilities.FormatTimeSpan(TimeSpan.Zero);
+						}
+						else if (this.OverallEncodeProgressFraction >= 0 && this.OverallEncodeProgressFraction <= 1.01)
+						{
+							if (this.OverallEncodeProgressFraction == 0)
+							{
+								this.overallEtaSpan = TimeSpan.MaxValue;
+							}
+							else
+							{
+								try
+								{
+									this.overallEtaSpan =
+										TimeSpan.FromSeconds((long)(((1.0 - this.OverallEncodeProgressFraction) * queueElapsedSeconds) / this.OverallEncodeProgressFraction));
+								}
+								catch (OverflowException)
+								{
+									this.overallEtaSpan = TimeSpan.MaxValue;
+								}
+							}
 
-					    this.EstimatedTimeRemaining = Utilities.FormatTimeSpan(this.overallEtaSpan);
-				    }
-				    else
-				    {
-						// Something fishy is going on. Dump the queue work contents and all relevant variables.
-					    if (!this.diagnosticsLogged)
-					    {
-						    IAppLogger logger = StaticResolver.Resolve<IAppLogger>();
+							this.EstimatedTimeRemaining = Utilities.FormatTimeSpan(this.overallEtaSpan);
+						}
+						else
+						{
+							// Something fishy is going on. Dump the queue work contents and all relevant variables.
+							if (!this.diagnosticsLogged)
+							{
+								IAppLogger logger = StaticResolver.Resolve<IAppLogger>();
 
-							logger.LogError("Invalid total progress fraction calculated." + Environment.NewLine +
-							                $"Total queue cost: {this.totalQueueCost}" + Environment.NewLine +
-											$"Completed queue work: {this.completedQueueWork}" + Environment.NewLine +
-											$"In progress work: {inProgressJobsCompletedWork}" + Environment.NewLine +
-											$"Completed items: {this.completedItems}");
-						    for (int i = 0; i < this.queuedWork.Count; i++)
-						    {
-							    JobWork workItem = this.queuedWork[i];
-							    logger.LogError($"Queue item {i}: Cost: {workItem.Cost} Video Length: {workItem.JobVideoLength} Completed: {i < this.completedItems}");
-						    }
+								logger.LogError("Invalid total progress fraction calculated." + Environment.NewLine +
+												$"Total queue cost: {this.totalQueueCost}" + Environment.NewLine +
+												$"Completed queue work: {this.completedQueueWork}" + Environment.NewLine +
+												$"In progress work: {inProgressJobsCompletedWork}" + Environment.NewLine +
+												$"Completed items: {this.completedItems}");
+								for (int i = 0; i < this.queuedWork.Count; i++)
+								{
+									JobWork workItem = this.queuedWork[i];
+									logger.LogError($"Queue item {i}: Cost: {workItem.Cost} Video Length: {workItem.JobVideoLength} Completed: {i < this.completedItems}");
+								}
 
-						    this.diagnosticsLogged = true;
-					    }
+								this.diagnosticsLogged = true;
+							}
 
-					    this.EstimatedTimeRemaining = Utilities.FormatTimeSpan(TimeSpan.Zero);
+							this.EstimatedTimeRemaining = Utilities.FormatTimeSpan(TimeSpan.Zero);
+						}
 					}
 				}
-		    }
 
-		    this.OverallEncodeTime = this.elapsedQueueEncodeTime.Elapsed;
-		    this.OverallEta = this.overallEtaSpan;
+				this.OverallEncodeTime = this.elapsedQueueEncodeTime.Elapsed;
+				this.OverallEta = this.overallEtaSpan; 
+			}
 		}
     }
 }
