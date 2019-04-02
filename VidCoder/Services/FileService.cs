27269851f2d44977ef42c170dbc1fb8acd1fe5ef
RandomEngy <david.rickard@gmail.com>
2019-04-02 00:18:22 -0700

    Added logging and fallback behavior for issues with initial directory when opening a file.

--- VidCoder/Services/FileService.cs
+++ VidCoder/Services/FileService.cs
@@ -25,67 +25,50 @@ namespace VidCoder.Services
 
 		public IList<string> GetFileNames(string initialDirectory)
 		{
-			var dialog = new Microsoft.Win32.OpenFileDialog();
-			dialog.Multiselect = true;
-
-			if (!string.IsNullOrEmpty(initialDirectory))
-			{
-				try
+			var dialogResult = ShowOpenDialogWithInitialDirectory(
+				() =>
 				{
-					string fullDirectory = Path.GetFullPath(initialDirectory);
+					var dialog = new Microsoft.Win32.OpenFileDialog();
+					dialog.Multiselect = true;
 
-					if (Directory.Exists(fullDirectory))
-					{
-						dialog.InitialDirectory = fullDirectory;
-					}
-				}
-				catch (NotSupportedException)
-				{
-					StaticResolver.Resolve<IAppLogger>().Log("Could not recognize initial directory " + initialDirectory);
-				}
-			}
+					return dialog;
+				},
+				initialDirectory);
 
-			bool? result = dialog.ShowDialog();
-			if (result == false)
+			if (dialogResult.result == false)
 			{
 				return null;
 			}
 
-			return new List<string>(dialog.FileNames);
+			return new List<string>(dialogResult.createdDialog.FileNames);
 		}
 
 		public string GetFileNameLoad(string initialDirectory = null, string title = null, string filter = null)
 		{
-			var dialog = new Microsoft.Win32.OpenFileDialog();
-
-			if (title != null)
-			{
-				dialog.Title = title;
-			}
-
-			if (filter != null)
-			{
-				dialog.Filter = filter;
-			}
-
-			if (!string.IsNullOrEmpty(initialDirectory))
-			{
-				string fullDirectory = Path.GetFullPath(initialDirectory);
-
-				if (Directory.Exists(fullDirectory))
+			var dialogResult = ShowOpenDialogWithInitialDirectory(
+				() =>
 				{
-					dialog.InitialDirectory = fullDirectory;
-				}
-			}
+					var dialog = new Microsoft.Win32.OpenFileDialog();
+					if (title != null)
+					{
+						dialog.Title = title;
+					}
 
-			bool? result = dialog.ShowDialog();
+					if (filter != null)
+					{
+						dialog.Filter = filter;
+					}
 
-			if (result == false)
+					return dialog;
+				},
+				initialDirectory);
+
+			if (dialogResult.result == false)
 			{
 				return null;
 			}
 
-			return dialog.FileName;
+			return dialogResult.createdDialog.FileName;
 		}
 
 		public string GetFileNameSave(string initialDirectory = null, string title = null, string initialFileName = null, string defaultExt = null, string filter = null)
@@ -198,6 +181,46 @@ namespace VidCoder.Services
 			catch (Win32Exception)
 			{
 				MessageBox.Show(MainRes.VideoPlayError);
+			}
+		}
+
+		private static (bool? result, Microsoft.Win32.OpenFileDialog createdDialog) ShowOpenDialogWithInitialDirectory(Func<Microsoft.Win32.OpenFileDialog> dialogFunc, string initialDirectory)
+		{
+			var dialog = dialogFunc();
+
+			if (!string.IsNullOrEmpty(initialDirectory))
+			{
+				try
+				{
+					string fullDirectory = Path.GetFullPath(initialDirectory);
+
+					if (Directory.Exists(fullDirectory))
+					{
+						dialog.InitialDirectory = fullDirectory;
+					}
+				}
+				catch (NotSupportedException)
+				{
+					StaticResolver.Resolve<IAppLogger>().Log("Could not recognize initial directory " + initialDirectory);
+				}
+			}
+
+			try
+			{
+				return (dialog.ShowDialog(), dialog);
+			}
+			catch (Exception exception)
+			{
+				if (initialDirectory != null)
+				{
+					StaticResolver.Resolve<IAppLogger>().Log($"Error showing file dialog with initial directory '{initialDirectory}'" + Environment.NewLine + exception);
+					return ShowOpenDialogWithInitialDirectory(dialogFunc, null);
+				}
+				else
+				{
+					StaticResolver.Resolve<IAppLogger>().Log($"Error showing file dialog with no initial directory" + Environment.NewLine + exception);
+					return (false, null);
+				}
 			}
 		}
 	}
