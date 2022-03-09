b06fee85bd0ec2790a7138a144587443a78efc26
David Rickard <davricka@microsoft.com>
2022-03-08 23:36:54 -0800

    Suppressed platform compat warning as we don't have other platforms.

--- VidCoderWorker/HandBrakeWorkerBase.cs
+++ VidCoderWorker/HandBrakeWorkerBase.cs
@@ -202,7 +202,9 @@ namespace VidCoderWorker
 					affinityMask |= (uint)(1 << i);
 				}
 
+#pragma warning disable CA1416 // Validate platform compatibility
 				process.ProcessorAffinity = (IntPtr)affinityMask;
+#pragma warning restore CA1416 // Validate platform compatibility
 			}
 		}
 
