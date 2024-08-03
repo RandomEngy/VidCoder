3c563c8dda4d8608f75f6da31871b289c5d7867c
David Rickard <davricka@microsoft.com>
2024-08-03 14:42:45 -0700

    Fixed possible crash on selecting a preset or picker dropdown item.

--- VidCoder/Controls/EnhancedListView.cs
+++ VidCoder/Controls/EnhancedListView.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.AnyContainer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using VidCoder.Services;
 
 namespace VidCoder.Controls;
 
@@ -15,25 +17,32 @@ public class EnhancedListView : ListView
 	{
 		base.OnSelectionChanged(e);
 
-		bool isVirtualizing = VirtualizingPanel.GetIsVirtualizing(this);
-		bool isMultiSelect = this.SelectionMode != SelectionMode.Single;
-
-		if (isVirtualizing && isMultiSelect)
+		try
 		{
-			var newSelectedItems = this.SelectedItems.Cast<IListItemViewModel>().ToList();
+			bool isVirtualizing = VirtualizingPanel.GetIsVirtualizing(this);
+			bool isMultiSelect = this.SelectionMode != SelectionMode.Single;
 
-			foreach (var deselectedItem in this.selectedItems.Except(newSelectedItems))
+			if (isVirtualizing && isMultiSelect)
 			{
-				deselectedItem.IsSelected = false;
-			}
+				var newSelectedItems = this.SelectedItems.Cast<IListItemViewModel>().ToList();
 
-			this.selectedItems.Clear();
-			this.selectedItems.AddRange(newSelectedItems);
+				foreach (var deselectedItem in this.selectedItems.Except(newSelectedItems).ToList())
+				{
+					deselectedItem.IsSelected = false;
+				}
 
-			foreach (var newlySelectedItem in this.selectedItems)
-			{
-				newlySelectedItem.IsSelected = true;
+				this.selectedItems.Clear();
+				this.selectedItems.AddRange(newSelectedItems);
+
+				foreach (var newlySelectedItem in this.selectedItems)
+				{
+					newlySelectedItem.IsSelected = true;
+				}
 			}
+		}
+		catch (Exception exception)
+		{
+			StaticResolver.Resolve<IAppLogger>().Log("Could not select item: " + exception.ToString());	
 		}
 	}
 }
